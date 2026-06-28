86336e368558c4e76d08835fbd24d85d86f42ecb
Amir SABBAG <amir.sabbag@polytechnique.edu>
2026-06-28 14:01:21 +0200

    Fix example

--- examples/sphere.rs
+++ examples/sphere.rs
@@ -1,7 +1,7 @@
 use core::f64::consts::PI;
 
-use renderer::{ImageRenderer, Material, SceneBuilder, SphereBuilder};
-use renderer::{RenderConfig, SceneConfig, render_config, scene_config};
+use sotte::{ImageRenderer, Material, SceneBuilder, SphereBuilder};
+use sotte::{RenderConfig, SceneConfig, render_config, scene_config};
 
 render_config!(RConf, width: 512, height: 512, samples: 1000);
 scene_config!(SConf);
